7085ae2a89f104a9daf10e855c3d3956a155e7d0
Karim Daouas <karim.daouas@mines-albi.fr>
2024-08-03 12:01:12 +0200

    User Component Update

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
+let i = 0;
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,6 +10,33 @@ import { DUMMY_USERS } from '../dummy-users';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
+/* Premiére magnière d'update le dom 
 export class UserComponent {
-  selectedUser = DUMMY_USERS[0]
+  selectedUser = DUMMY_USERS[i];
+
+  get imagePath(){
+    return './assets/users/'+ this.selectedUser.avatar;
+  }
+
+  onSelectUser(){
+    if(i<DUMMY_USERS.length)
+      i=i+1;
+    else
+      i=0;
+    this.selectedUser = DUMMY_USERS[i];
+  }
+}*/
+//Deuxième manière avec Signal
+export class UserComponent {
+  selectedUser = signal(DUMMY_USERS[i]);
+
+  imagePath = computed(()=> './assets/users/'+ this.selectedUser().avatar)
+
+  onSelectUser(){
+    if(i<DUMMY_USERS.length)
+      i=i+1;
+    else
+      i=0;
+    this.selectedUser.set(DUMMY_USERS[i]);
+  }
 }
